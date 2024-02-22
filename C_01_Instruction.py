83a0cb7484388097e983febb8a2d3760977b2d11
sonk2229 <159740652+sonk2229@users.noreply.github.com>
2024-02-23 10:51:45 +1300

    roll it 13 files

---
+++ C_01_Instruction.py
@@ -0,0 +1,16 @@
+
+print("🎲🎲Roll it 13 🎲🎲")
+
+
+# loop for testing purposes
+
+while True:
+    want_instruction = input("Do you want to read the instruction?").lower()
+
+    if want_instruction == "yes":
+        print("you chose yes")
+    elif want_instruction == "no":
+        print("you chose no")
+    else:
+        print("You did not choose a valid response")
+

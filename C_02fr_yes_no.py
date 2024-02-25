315fdc67849b2dcbdd7aecebb17ccc3b3619e317
sonk2229 <159740652+sonk2229@users.noreply.github.com>
2024-02-26 12:27:17 +1300

    Add files via upload

---
+++ C_02fr_yes_no.py
@@ -0,0 +1,16 @@
+# checks users enter yes (y) or no (n)
+def yes_no(question):
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("You did not choose a valid response")
+
+
+# Main routine
+want_instructions = yes_no("Do you want to read the instructions?")
+print(f"you chose{want_instructions}")

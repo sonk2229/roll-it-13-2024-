04b750645181b1e1fa9412804978689e5f3f0da2
sonk2229 <159740652+sonk2229@users.noreply.github.com>
2024-02-26 12:27:54 +1300

    Add files via upload

--- C_01_Instruction.py
+++ C_01_Instruction.py
@@ -1,15 +1,15 @@
-
 print("🎲🎲Roll it 13 🎲🎲")
 
 
 # loop for testing purposes
 
 while True:
-    want_instruction = input("Do you want to read the instruction?").lower()
+    want_instructions = input("Do you want to read the instruction?").lower()
 
-    if want_instruction == "yes":
+    # checks users enter yes (y) or no (n)
+    if want_instructions == "yes" or want_instructions == "y":
         print("you chose yes")
-    elif want_instruction == "no":
+    elif want_instructions == "no" or want_instructions == "n":
         print("you chose no")
     else:
         print("You did not choose a valid response")

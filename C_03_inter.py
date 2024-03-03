2ed951bc8ccc722f7f4bcd987c44e0893dbaeb0d
sonk2229 <159740652+sonk2229@users.noreply.github.com>
2024-03-04 10:34:12 +1300

    Add files via upload

---
+++ C_03_inter.py
@@ -0,0 +1,17 @@
+
+while True:
+
+    error = "Please enter an integer that is 13 or more."
+
+    try:
+        my_num = int(input("Enter an integer"))
+
+       # checks that the number is more than / equal to 13
+        if my_num <13:
+            print(error)
+        else:
+            print("Your number is ", my_num)
+
+    except ValueError:
+        print(error)
+
